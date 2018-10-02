e13bb958d375bd643de9aa35563dcf596ac66b5c
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2018-10-02 09:47:22 +0200

    de: Acronyms

---
+++ lang/de/attendance.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'attendance', language 'de'
+ *
+ * @package   mod_attendance
+ * @copyright 2018 Liip
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['Aacronym'] = 'U';
+$string['Afull'] = 'Unentschuldigt';
+$string['Eacronym'] = 'E';
+$string['Efull'] = 'Entschuldigt';
+$string['Lacronym'] = 'V';
+$string['Lfull'] = 'Verspätet';
+$string['Pacronym'] = 'A';
+$string['Pfull'] = 'Anwesend';
